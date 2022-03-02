bd54f02521f11f2db66e14c1d03fd9b3bd54a439
thangzjm99 <60538008+thangzjm99@users.noreply.github.com>
2022-03-02 16:25:24 +0700

    PRO 6437 : create db

--- Crud 3-2-2022/alobridge_crud.sql
+++ Crud 3-2-2022/alobridge_crud.sql
@@ -1,21 +1,21 @@
 
-CREATE TABLE `product` (
+CREATE TABLE `products` (
   `id` int(12) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `description` varchar(255) NOT NULL,
   `quantity` int(20) NOT NULL,
   `price` int(20) NOT NULL,
-  `is_deleted` tinyint(1) NOT NULL,
+  `is_deleted` boolean NOT NULL,
   PRIMARY KEY (`id`)
 ) ;
 
 
 -- 
-CREATE TABLE `tag` (
+CREATE TABLE `tags` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
-  `is_deleted` tinyint(1) NOT NULL,
+  `is_deleted` boolean NOT NULL,
   PRIMARY KEY (`id`)
 ) ;
 
@@ -23,20 +23,21 @@ CREATE TABLE `tag` (
 CREATE TABLE `product_tag` (
   `product_id` int(20) NOT NULL,
   `tag_id` int(20) NOT NULL,
+  PRIMARY KEY (product_id,tag_id),
   CONSTRAINT `product_id` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `tag_id` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ;
 
 
 
-CREATE TABLE `user` (
+CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(11) NOT NULL,
   `password` varchar(11) NOT NULL,
   `fullname` varchar(11) NOT NULL,
   `phone` int(11) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `is_deleted` tinyint(1) NOT NULL,
+  `is_deleted` boolean NOT NULL,
   PRIMARY KEY (`id`)
 ) ;
 
