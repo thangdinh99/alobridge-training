3758d27b63c3ccc474a6cdae43b0759632e8b395
thangdinh99 <100657238+thangdinh99@users.noreply.github.com>
2022-03-03 15:30:30 +0700

    PRO 6443 : Giao dien login va sua lai db (#3)

---
+++ Crud-3-2-2022/index.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="./css/styles.css">
+	<title>Login</title>
+</head>
+<body>
+	<div class="container" >
+		<div class="form-container log-in-container">
+			<form action="" method="post">
+				<h1>Login</h1>
+				<input type="text" name="username" placeholder="Username" />
+				<input type="password" name="password" placeholder="Password" />
+            <div class="remember-me">
+               <input type="checkbox" name="remember" id="remember" />
+               <label for="remember">Remember me</label>
+            </div>
+				<button type="submit" name="login">Log In</button>
+			</form>
+		</div>
+		<div class="image-container">
+			<img src="https://picsum.photos/seed/picsum/500">
+		</div>
+	</div>
+</body>
+</html>
